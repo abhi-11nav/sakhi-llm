90a14d7d8aa7cda756724c815ff3e3107709e2d8
Abhinav Reddy Mandli <raabhinav65@gmail.com>
2025-08-15 21:48:01 -0300

    more stable implementation of RMSNORM

--- sakhilabs/model/components/rms_norm.py
+++ sakhilabs/model/components/rms_norm.py
@@ -1,14 +1,20 @@
+# https://github.com/openai/gpt-oss/blob/main/gpt_oss/torch/model.py
+
+
 import torch
 import torch.nn as nn
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, dim, eps=1e-6):
+    def __init__(self, dim, eps=1e-6, device=None):
         super().__init__()
+        self.dim = dim
         self.eps = eps
-        self.weight = nn.Parameter(torch.ones(dim))
+        self.weight = nn.Parameter(torch.ones(dim, device=device, dtype=torch.float32))
 
     def forward(self, x):
-        norm = x.norm(2, dim=-1, keepdim=True)
-        rms = norm / (x.shape[-1] ** 0.5)
-        return self.weight * x / (rms + self.eps)
+        assert x.shape[-1] == self.dim
+        orig_dtype = x.dtype
+        t = x.float()
+        t = t * torch.rsqrt(torch.mean(t**2, dim=-1, keepdim=True) + self.eps)
+        return (t * self.weight).to(orig_dtype)
